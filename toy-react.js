bc379419d4b08e4ca4043d90f9781addc122675f
luyanfei <luyanfei78@163.com>
2020-08-21 16:56:36 +0800

    add update.

--- toy-react.js
+++ toy-react.js
@@ -1,5 +1,14 @@
 const RENDER_TO_DOM = Symbol("render to dom")
 
+function replaceContent(range, node) {
+    range.insertNode(node)
+    range.setStartAfter(node)
+    range.deleteContents()
+
+    range.setStartBefore(node)
+    range.setEndAfter(node)
+}
+
 export class Component {
     constructor() {
         this.props = Object.create(null)
@@ -15,11 +24,15 @@ export class Component {
     }
     [RENDER_TO_DOM](range) {
         this._range = range
-        this.render()[RENDER_TO_DOM](range)
+        //render时的vdom要保留下来，更新时与最新的vdom来比对
+        this._vdom = this.vdom
+        this._vdom[RENDER_TO_DOM](range)
     }
     get vdom() {
+        this.vchildren = this.children.map(child => child.vdom)
         return this.render().vdom
     }
+    /*
     rerender() {
         //解决range合并的问题，但在我的机器上并未出现这个问题。
         let oldRange = this._range
@@ -32,10 +45,68 @@ export class Component {
         oldRange.setStart(range.endContainer, range.endOffset)
         oldRange.deleteContents()
     }
+    */
+
+    update() {
+        let isSameNode = (oldNode, newNode) => {
+            if(oldNode.type !== newNode.type) {
+                return false
+            }
+            for(let name in newNode.props) {
+                if(newNode.props[name] !== oldNode.props[name]) {
+                    return false
+                }
+            }
+            if(Object.keys(oldNode.props).length > Object.keys(newNode.props).length) {
+                return fasle
+            }
+            if(newNode.type === '#text') {
+                if(newNode.content !== oldNode.content) {
+                    return false
+                }
+            }
+            return true
+        }
+        let update = (oldNode, newNode) => {
+            //需要比较的内容: type, props, children
+            //#text content
+            if(!isSameNode(oldNode, newNode)) {
+                newNode[RENDER_TO_DOM](oldNode._range)
+                return
+            }
+            newNode._range = oldNode._range
+
+            let newChildren = newNode.vchildren
+            let oldChildren = oldNode.vchildren
+
+            if(!newChildren || !newChildren.length) {
+                return
+            }
+
+            let tailRange = oldChildren[oldChildren.length -1]._range
+            for(let i = 0; i < newChildren.length; i++) {
+                let newChild = newChildren[i]
+                let oldChild = oldChildren[i]
+                if(i < oldChildren.length) {
+                    update(oldChild, newChild)
+                } else {
+                    let range = document.createRange()
+                    range.setStart(tailRange.endContainer, tailRange.endOffset)
+                    range.setEnd(tailRange.endContainer, tailRange.endOffset)
+                    newChild[RENDER_TO_DOM](range)
+                    tailRange = range
+                }
+            }
+
+        }
+        let vdom = this.vdom
+        update(this._vdom, vdom)
+        this._vdom = vdom
+    }
     setState(newState) {
         if(this.state === null || typeof this.state !== 'object') {
             this.state = newState
-            this.rerender()
+            this.update()
             return
         }
         let merge = (oldState, newState) => {
@@ -48,7 +119,7 @@ export class Component {
             }
         }
         merge(this.state, newState)
-        this.rerender()
+        this.update()
     }
 }
 
@@ -78,15 +149,12 @@ class ElementWrapper extends Component{
     }
     */
     get vdom() {
+        this.vchildren = this.children.map(child => child.vdom)
         return this
     }
 
-    get vchildren() {
-        return this.children.map(child => child.vdom)
-    }
-
     [RENDER_TO_DOM](range) {
-        range.deleteContents()
+        this._range = range
 
         let root = document.createElement(this.type)
 
@@ -109,7 +177,7 @@ class ElementWrapper extends Component{
             childRange.setEnd(root, root.childNodes.length)
             child[RENDER_TO_DOM](childRange)    
         }
-        range.insertNode(root)
+        replaceContent(range, root)
     }
 }
 
@@ -118,14 +186,14 @@ class TextWrapper extends Component{
         super(content)
         this.content = content
         this.type = '#text'
-        this.root = document.createTextNode(content)
     }
     get vdom() {
         return this
     }
     [RENDER_TO_DOM](range) {
-        range.deleteContents()
-        range.insertNode(this.root)
+        this._range = range
+        let root = document.createTextNode(this.content)
+        replaceContent(range, root)
     }
 }
 
